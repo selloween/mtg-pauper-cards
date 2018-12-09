e8640d8cb6959069ad09ffddc5ecb3bd5c84ef14
Selwyn Rogers <35191120+selloween@users.noreply.github.com>
2018-12-09 20:18:50 +0100

    add sideboard cards, refactor and clean up

--- nightmare.js
+++ nightmare.js
@@ -1,61 +1,60 @@
 const Nightmare = require('nightmare')
+const fs = require('fs')
+const path = require("path")
 const nightmare = Nightmare({ show: true })
-const fs = require('fs');
-const path = require("path");
+const selector = '.W14, .L14'
+const timeFrame = '110'
 
-//const selector = '.L14 , .W14'
-const filePath = path.normalize(__dirname + "/data.txt");
+const getData = (selector, timeFrame, pageCount, sideboard) => {
 
-const writeFile = (filePath, input) => {
-  return new Promise((resolve, reject ) => {
+  let fileName = '/mainboard.txt'
+  let url = 'https://mtgtop8.com/topcards?f=PAU&meta=' + timeFrame + '&current_page=' + pageCount
 
-    fs.appendFile(filePath, (result + "\r\n"),(err) => {
-      if (err) throw err
+  if (sideboard === true) {
+    fileName =  '/sideboard.txt'
+    url = 'https://mtgtop8.com/topcards?f=PAU&meta=' + timeFrame + '&current_page=' + pageCount + '&maindeck=SB'
+  }
 
-      resolve(console.log('Saved!'));
-    })
+  let filePath = path.normalize(__dirname + fileName)
 
-  });
-}
-const getData = (selector, period, count) => {
+  console.log(pageCount)
 
-  console.log(count)
-
-  nightmare
-    .goto('https://mtgtop8.com/topcards?f=PAU&meta=' + period + '&current_page=' + count + '&maindeck=SB')
-    .evaluate(selector => {
-        return Array.from(document.querySelectorAll(selector)).map(element => element.innerText);
-      }, selector)
-    .then( results => {
-
-      let i = 0;
-      //let seperator = ",";
-
-      results.forEach( (result) => {
-        i ++
-
-        if ( i % 3 ===  1 ) {
-          console.log(result);
-
-          fs.appendFile(filePath, (result + "\r\n"),(err) => {   
-            if (err) throw err
-          })
-
+  if (selector && timeFrame && pageCount) {
+    nightmare
+      .goto(url)
+      .exists('.Nav_PN_no')
+      .then((res) => {
+        if(res === true && pageCount > 1) {
+          if(sideboard === false) {
+            console.log('Finished Maindeck.')
+            getData(selector, timeFrame, 1, true)
+          }
+        } else {
+          nightmare
+            .evaluate(selector => {
+                return Array.from(document.querySelectorAll(selector)).map(element => element.innerText)
+              }, selector)
+            .then( results => {
+              let i = 0;
+              results.forEach( (result) => {
+                i ++
+                if ( i % 3 ===  1 ) {
+                  console.log(result);
+                  fs.appendFile(filePath, (result + "\r\n"),(err) => {   
+                    if (err) throw err
+                  })
+                }
+              })
+            })
+            .then(() => {
+                pageCount++
+                getData(selector, timeFrame, pageCount, sideboard)
+            })
+            .catch(error => {
+              console.error('Search failed:', error)
+            })
         }
-   
-      })
-    })
-    .then(() => {
-      console.log(count)
-      if(count <= 49) {
-        console.log('click')
-        count++
-        getData('.W14, .L14', '110', count)
-      }
-    })
-    .catch(error => {
-      console.error('Search failed:', error)
-    })
-} 
-
-getData('.W14, .L14', '110', 1);
+      })  
+  }
+}
+getData(selector, timeFrame, 1, false)
